e28b816b5351d6ac43e4d20196ff3e38a99be195
Rkyaly <Rkyaly@163.com>
2025-10-21 10:08:39 +0800

    1

--- scripts/AuroraRoutines/Main.lua
+++ scripts/AuroraRoutines/Main.lua
@@ -6,3 +6,15 @@ local Spellbook = require("AuroraRoutines/Routines/Warrior/specialisation/Spellb
 -- 使用Spellbook模块中的Aurora对象
 local Aurora = Spellbook.Aurora
 _G.Aurora = Aurora -- 将Aurora对象设置为全局变量，以便其他模块使用
+
+local spells = Aurora.SpellHandler.Spellbooks.warrior["3"].day01.spells
+local auras = Aurora.SpellHandler.Spellbooks.warrior["3"].day01.auras
+local talents = Aurora.SpellHandler.Spellbooks.warrior["3"].day01.talents
+
+local spell = Aurora.SpellHandler.NewSpell(106785, {
+    ignoreFacing = true,
+    ignoreMoving = true,
+    isSkillshot = true,
+    radius = 8,
+    maxOffset = 30
+})
--- scripts/AuroraRoutines/Routines/Warrior/specialisation/Spellbook.lua
+++ scripts/AuroraRoutines/Routines/Warrior/specialisation/Spellbook.lua
@@ -1,7 +1,11 @@
 -- Create your spells
+-- 定义一个局部变量 NewSpell，它是 Aurora.SpellHandler.NewSpell 的引用
 local NewSpell = Aurora.SpellHandler.NewSpell
+-- 调用 Aurora.SpellHandler.PopulateSpellbook 方法填充技能书
 Aurora.SpellHandler.PopulateSpellbook({
+-- 定义一个名为spells的表，包含多个法术技能
     spells = {
+    -- AutoAttack技能，使用NewSpell函数创建，技能ID为106785
         AutoAttack = NewSpell(106785),
 --        Charge = NewSpell(100),
 --        ShieldSlam = NewSpell(23922),
